545e67068bcbcf7e3c8f1415f78ed555398eb229
marinimaster <villeda.mm02@gmail.com>
2026-01-28 20:29:09 -0600

    changed query selection in backend/index.js to match login table

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.post("/login", async (req, res) => {
 
   try {
     const result = await pool.query(
-      "SELECT * FROM users WHERE email = $1 AND password = $2",
+      "SELECT * FROM logins WHERE email = $1 AND password = $2",
       [email, password]
     );
 
